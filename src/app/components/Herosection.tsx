4ef279d7e509349935786482c3353984963509f6
Tamalika Bhowal <tamalikabhowal@gmail.com>
2024-09-30 18:46:46 +0530

    first commit

---
+++ src/app/components/Herosection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Herosection = () => {
+  return (
+    <>
+    <section> 
+    <h1>Hello its me tamalika</h1>
+    </section>
+    </>
+  )
+}
+
+export default Herosection
